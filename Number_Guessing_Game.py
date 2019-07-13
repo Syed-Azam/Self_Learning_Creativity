c0a0bdd1c44445bf76103969e36430b1df5e1bb8
Syed Azam Hassan <kashif.hassan@gmail.com>
2019-07-13 15:30:03 +0500

    Number Guessing Game Updated

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -2,12 +2,15 @@
 from time import sleep # to shows numbers for few milliseconds
 from os import system # to use clear screen
 from random import randint # to generate random numbers
-numbers = [str(randint(1,4)) for n in range(4)]
-for n in range(4):
+import winsound
+
+numbers = [str(randint(1,6)) for n in range(6)]
+input("I will show u 6 numbers. Press Enter to Begin.")
+for n in range(6):
     system("cls")
-    print (' '*n+str(numbers[n]), end='')
-    print ("\a")
-    sleep(0.4)
+    print (' '*n+str(numbers[n]), end='', flush=True)
+    winsound.Beep(n*500+1000, 100)
+    sleep(0.2)
 system("cls")
 g = input("Guess the numbers : ")
 if g == ''.join(numbers):
@@ -15,3 +18,5 @@ if g == ''.join(numbers):
 else:
     print("You Lose!!!!")
     print("It was : "+''.join(numbers))
+	
+input()
